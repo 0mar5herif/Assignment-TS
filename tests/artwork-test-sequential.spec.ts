6c0f973c5c9e6606c57bd4eb42d4d8a6d7f54b86
0mar5herif <143499692+0mar5herif@users.noreply.github.com>
2024-11-07 12:29:40 +0200

    Update artwork-test-sequential.spec.ts

--- tests/artwork-test-sequential.spec.ts
+++ tests/artwork-test-sequential.spec.ts
@@ -13,9 +13,9 @@ const password = 'nhk9dad2EQW!xae_bpm'
 const apikey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE'
 const bearer = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE'
 
-test('Submit, list view, and write review', async ({ page }) => {
+test('Submit artwork; find in list view; write review', async ({ page }) => {
 
-  const loginPage = new LoginPage(page);
+  //const loginPage = new LoginPage(page);
   const artworkPage = new ArtworkPage(page);
   const dataGen = new DataGenerator();
   const logintokenAPI = new LoginTokenAPI(page);
